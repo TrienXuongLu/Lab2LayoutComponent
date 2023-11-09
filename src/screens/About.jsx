77dd5a14f1cd4e7d6345948f42cf5c23958eb330
Trien Xuong Lu <lutrienxuong@gmail.com>
2023-11-09 09:15:05 -0700

    Little change update

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import MainLayout from '../layouts/MainLayout';
 
-function About() {
+function About({navigation}) {
     return (
         <MainLayout>
             <View>
                 <Text>Simon's Awesome To Do List App</Text>
                 <Text>Create by: Trien Xuong Lu</Text>
                 <Text>Version: 0.1.215241</Text>
+                <Button title="Home Page" onPress={() => navigation.navigate('Home')} />
             </View>
         </MainLayout>
     );
